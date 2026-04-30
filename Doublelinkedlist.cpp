3e19266991b02dac05d4f97efbf2364cbdcbff30
kalycaokta-ux <kalycaokta@gmail.com>
2026-04-30 12:08:29 +0700

    menambahkan int rollNo

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -84,4 +84,15 @@ public:
     }
 
     void hapus()
+    {
+        if (START == NULL)
+        {
+            cout << "\nList is empty" << endl;
+            return;
+        }
+
+        cout << "\nEnter the roll number of the student whose record is to be deleted: ";
+        int rollNo;
+        cin >> rollNo;
+    }
 }
